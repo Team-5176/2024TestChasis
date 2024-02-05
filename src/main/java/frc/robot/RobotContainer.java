9856f37f9ef455e6ff4a4c70496f5e3cf4b62698
SLUH Robotics SSH Account <robotics@sluh.org>
2024-02-05 11:21:52 -0600

    creating a test path

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -121,7 +121,7 @@ public class RobotContainer
   public Command getAutonomousCommand()
   {
     // An example command will be run in autonomous
-    return drivebase.getAutonomousCommand("New Path", true);
+    return drivebase.getAutonomousCommand("Test path", true);
   }
 
   public void setDriveMode()
