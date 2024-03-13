1457d4fcf3f91653b36a68158490c571daa9946e
original-name-creator <lukestoff@icloud.com>
2024-03-13 00:25:21 -0500

    climbers are simple if you're bad at them

--- src/main/java/frc/robot/subsystems/Climber/climberSubsystem.java
+++ src/main/java/frc/robot/subsystems/Climber/climberSubsystem.java
@@ -14,26 +14,20 @@ public class climberSubsystem extends SubsystemBase {
     private TalonFX leftTalon = new TalonFX(ClimberConstants.leftClimberMotor);
     private TalonFX rightTalon = new TalonFX(ClimberConstants.rightClimberMotor);
 
-    private boolean brakeEnabled = false;
-
     public climberSubsystem(){
-        brakeEnabled = false;
-    }
-
-    public void toggleBrake(){
-
-        brakeEnabled = !brakeEnabled;
-
-        if(brakeEnabled){
-            leftTalon.setControl(new StaticBrake());
-            rightTalon.setControl(new StaticBrake());
-        }
 
     }
 
     public void changeHeight(DoubleSupplier leftClimberAxis, DoubleSupplier rightClimberAxis){
-        leftTalon.set(leftClimberAxis.getAsDouble());
-        rightTalon.set(rightClimberAxis.getAsDouble());
+
+        if(Math.abs(leftClimberAxis.getAsDouble()) > 0 || Math.abs(rightClimberAxis.getAsDouble()) > 0){
+            leftTalon.set(leftClimberAxis.getAsDouble());
+            rightTalon.set(rightClimberAxis.getAsDouble());
+            }
+        else{
+            leftTalon.setControl(new StaticBrake());
+            rightTalon.setControl(new StaticBrake());
+        }
     }
 
 }
